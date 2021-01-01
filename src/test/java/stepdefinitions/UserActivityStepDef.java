23b4bf83422181dcb15deba87fc899995713158c
Berati Z <berati@gmail.com>
2021-01-01 22:00:58 +0100

    user activity feature file crated

---
+++ src/test/java/stepdefinitions/UserActivityStepDef.java
@@ -0,0 +1,27 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class UserActivityStepDef {
+    @Given("user is on home page")
+    public void userIsOnHomePage() {
+    }
+
+    @And("user clicks Vor Ort on righ side element")
+    public void userClicksVorOrtOnRighSideElement() {
+    }
+
+    @And("user enters PLZ in the textbox which is bottom of the page and hat PLZ placeholder")
+    public void userEntersPLZInTheTextboxWhichIsBottomOfThePageAndHatPLZPlaceholder() {
+    }
+
+    @And("user clicks search button")
+    public void userClicksSearchButton() {
+    }
+
+    @Then("user verifies that shops list")
+    public void userVerifiesThatShopsList() {
+    }
+}
